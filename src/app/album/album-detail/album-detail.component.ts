55572d7324e4a589322d3181009f7912fda3dc3b
nemo1001 <phamhuyduc11094@gmail.com>
2019-10-07 11:28:08 +0700

    add song-detail

--- src/app/album/album-detail/album-detail.component.ts
+++ src/app/album/album-detail/album-detail.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {Playlist} from '../../model/playlist';
+import {Subscription} from 'rxjs';
+import {PlaylistService} from '../../service/playlist.service';
+import {SongService} from '../../service/song.service';
+import {ActivatedRoute} from '@angular/router';
+import {AddSongToPlaylistService} from '../../service/add-song-to-playlist.service';
+import {Album} from '../../model/album';
+
 
 @Component({
   selector: 'app-album-detail',
@@ -6,10 +14,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album-detail.component.scss']
 })
 export class AlbumDetailComponent implements OnInit {
+  private album: Album;
+  private message;
+  private songList: any[];
+  private playList: Playlist;
+  private subscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private playlistService: PlaylistService,
+    private songService: SongService,
+    private route: ActivatedRoute,
+    private addSongToPlaylistService: AddSongToPlaylistService
+  ) {
+  }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.subscription = this.playlistService.getPlayListDetail(id).subscribe(
+      result => {
+        if (result != null) {
+          this.playList = result;
+
+          this.playList.isDisabled = false;
+          this.songList = this.playList.songs;
+
+        }
+        console.log(this.playList);
+      }, error => {
+        this.message = 'Cannot retrieve Playlist . Cause: ' + error.message;
+      }
+    );
+  }
+  addToPlaylist(song) {
+    song.isDisabled = true;
+    this.addSongToPlaylistService.emitChange(song);
   }
 
 }
