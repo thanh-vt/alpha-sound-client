47850924d30a53c11b7a1d523bba473b1b73bfc8
pysga <pysga1996@gmail.com>
2019-10-22 01:59:25 +0700

    update ui

---
+++ src/app/user/profile/profile.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {FormBuilder} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthService} from '../../service/auth.service';
+import {ArtistService} from '../../service/artist.service';
+import {SongService} from '../../service/song.service';
+import {PlaylistService} from '../../service/playlist.service';
+import {PlayingQueueService} from '../../service/playing-queue.service';
+import {UserService} from '../../service/user.service';
+import {User} from '../../model/user';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit, OnDestroy {
+  currentUser: User;
+  message: string;
+  loading: boolean;
+  showEditForm = false;
+  subscription: Subscription = new Subscription();
+
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private router: Router,
+              private authService: AuthService, private artistService: ArtistService,
+              private songService: SongService, private playlistService: PlaylistService,
+              private playingQueueService: PlayingQueueService, private userService: UserService) {
+    this.userService.currentUser.subscribe(
+      currentUser => {
+        this.currentUser = currentUser;
+      }
+    );
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+}
