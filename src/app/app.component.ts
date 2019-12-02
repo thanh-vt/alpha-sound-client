e5f704a759c4f93f63b64bf98f953129e5d7db3f
pysga <pysga1996@gmail.com>
2019-12-02 23:14:22 +0700

    use oauth access token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {environment} from '../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Subscription} from 'rxjs';
 
 @Component({selector: 'app-root', templateUrl: 'app.component.html'})
 export class AppComponent {
+  subscription: Subscription = new Subscription();
 
-  constructor() {
+  @HostListener('window:beforeunload', ['$event'])
+  beforeunloadHandler(event) {
+    if (sessionStorage.getItem('userToken')) {
+      this.subscription.add(this.http.post(`${environment.authUrl}/token/revoke/${sessionStorage.getItem('userToken')}`, null)
+        .subscribe(
+          next => {console.log(next); },
+          error => {console.log(error); },
+          () => {}));
+    }
+  }
+
+  constructor(private http: HttpClient) {
   }
 }
