e8a0a96114e14194ab708bbbc374bc8db8080709
pysga <pysga1996@gmail.com>
2019-12-03 09:09:26 +0700

    use oauth access token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  authUrl: 'https://lambda-authorization-server.herokuapp.com/lambda-authorization/oauth/token',
+  authUrl: 'https://lambda-authorization-server.herokuapp.com/lambda-authorization',
   apiUrl: 'https://climax-sound.herokuapp.com/climax-sound/api',
+  clientId: 'fooClientIdPassword',
+  clientSecret: 'secret',
   production: true
 };
