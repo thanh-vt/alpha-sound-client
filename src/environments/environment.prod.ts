1ef028e7b41cfbac688beb69985856f86cf042c0
pysga <pysga1996@gmail.com>
2019-12-18 18:17:41 +0700

    update admin UI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   authUrl: 'https://lambda-authorization-server.herokuapp.com/lambda-authorization',
-  apiUrl: 'https://climax-sound.herokuapp.com/climax-sound/api',
+  apiUrl: 'https://lambda-climax-sound.herokuapp.com/climax-sound/api',
   clientId: 'fooClientIdPassword',
   clientSecret: 'secret',
   production: true
