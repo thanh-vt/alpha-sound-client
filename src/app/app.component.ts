96b0e4fbfdbbbbd4f322a2b01ebcd921940dd373
thanhvt <thanhvt@vissoft.vn>
2021-09-05 12:33:12 +0700

    fix cloud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserProfileService } from './service/user-profile.service';
 import { AuthService } from './service/auth.service';
 import { LocationStrategy } from '@angular/common';
--- src/app/helper/notification-interceptor.ts
+++ src/app/helper/notification-interceptor.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { TOAST_TYPE, VgToastService } from 'ngx-vengeance-lib';
 import { catchError } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class NotificationInterceptor implements HttpInterceptor {
-  constructor(private translate: TranslateService, private toastService: VgToastService) {}
+  private subject: Subject<HttpErrorResponse> = new Subject<HttpErrorResponse>();
+
+  constructor(private translate: TranslateService, private toastService: VgToastService) {
+    this.subject.subscribe(err => {
+      let msg: string;
+      if (err?.status === 0) {
+        msg = this.translate.instant('common.error.connection_problem');
+      } else {
+        msg = err?.error?.message ?? this.translate.instant('common.error.unknown');
+      }
+      this.toastService.show({ text: msg }, { type: TOAST_TYPE.ERROR });
+    });
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('second interceptor invoke: ', req.url);
     return next.handle(req).pipe(
-      catchError((error: HttpErrorResponse) => {
-        console.error(error);
-        let msg;
-        if (error.status === 0) {
-          msg = this.translate.instant('common.error.connection_problem');
-        } else {
-          msg = error?.error?.message ?? this.translate.instant('common.error.unknown');
-        }
-        this.toastService.show({ text: msg }, { type: TOAST_TYPE.ERROR });
-        throw error.error;
+      catchError((err: HttpErrorResponse) => {
+        this.subject.next(err);
+        throw err.error;
       })
     );
   }
