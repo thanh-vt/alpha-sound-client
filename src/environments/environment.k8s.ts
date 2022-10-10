b1508c729982005cda7ad93d6580c9de32e818bf
thanhvt <thanhvt@vissoft.vn>
2022-10-11 02:47:40 +0700

    fix bugs config

--- src/environments/environment.k8s.ts
+++ src/environments/environment.k8s.ts
@@ -1,31 +1,31 @@
 export const environment = {
-  authUrl: `http://${window.location.host}/api/v1/namespaces/vengeance/services/vengeance:80/proxy/lambda-auth`,
-  apiRootUrl: `http://${window.location.host}/api/v1/namespaces/vengeance/services/vengeance:80/proxy/alpha-sound`,
-  apiUrl: `http://${window.location.host}/api/v1/namespaces/vengeance/services/vengeance:80/proxy/alpha-sound/api`,
+  authUrl: `http://${window.location.host}/service/lambda-auth`,
+  apiRootUrl: `http://${window.location.host}/service/alpha-sound`,
+  apiUrl: `http://${window.location.host}/service/alpha-sound/api`,
   clientId: 'alpha_sound',
   clientSecret: '@lph@123',
   production: true,
-  baseHref: '/api/v1/namespaces/vengeance/services/vengeance:80/proxy/',
+  baseHref: '/',
   credMode: 'header', // cookie only available in https, otherwise set to header
   pingEndpointConfig: {
     ['0']: {
       'chi-discovery-service': {
-        url: `http://${window.location.host}/api/v1/namespaces/vengeance/services/vengeance:80/proxy/chi-discovery`
+        url: `http://${window.location.host}/service/chi-discovery/`
       }
     },
     ['1']: {
       'phi-config-service': {
-        url: `http://${window.location.host}/api/v1/namespaces/vengeance/services/vengeance:80/proxy/phi-config`
+        url: `http://${window.location.host}/service/phi-config/`
       }
     },
     ['2']: {
       'lambda-auth-service': {
-        url: `http://${window.location.host}/api/v1/namespaces/vengeance/services/vengeance:80/proxy/proxy`
+        url: `http://${window.location.host}/service/lambda-auth/`
       }
     },
     ['3']: {
       'alpha-sound-service': {
-        url: `http://${window.location.host}/api/v1/namespaces/vengeance/services/vengeance:80/proxy/alpha-sound`
+        url: `http://${window.location.host}/service/alpha-sound/`
       }
     }
   }
